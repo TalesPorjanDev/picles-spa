6c8f9d710e053754e0be8d957b024ddfdd0fa1e9
Tales Porjan <tales.fernandodev@gmail.com>
2024-04-15 20:23:47 -0300

    feat: foi alterado index,app,main tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,4 @@
 import { useState } from 'react'
-<<<<<<< HEAD
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
-=======
 
 export function App() {
   const [count, setCount] = useState(0)
@@ -45,4 +9,3 @@ export function App() {
     </button>
   )
 }
->>>>>>> 00f956eac8de704404fb546d69291c326a3be333
