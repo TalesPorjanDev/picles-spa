fd1441e5c2485570263409ee6329ac29613161a6
Tales Porjan <tales.fernandodev@gmail.com>
2024-04-16 20:07:17 -0300

    feat: instalando router dom

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,10 +1,13 @@
+import dog from '../../assets/dog.svg'
 import { Button, ButtonVariant } from "../../components/common/Button";
+import styles from './Home.module.css'
 
 export function Home() {
     return (
-      <>
+      <div className={styles.container}>
+        <img src ={dog} />
         <Button variant={ButtonVariant.Default}>Quero adotar</Button>
         <Button variant={ButtonVariant.Outlined}>Tenho um abrigo</Button>
-      </>
+      </div>
     )
 }
