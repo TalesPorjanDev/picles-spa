ef905b6aba9c61a9b6eb495ceaeeb201c70e41ce
Tales Porjan <tales.fernandodev@gmail.com>
2024-04-16 20:30:55 -0300

    feat: foi criado um redirecionamento para pagina inicial

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Pets } from "./pages/Pets";
 
 const router = createBrowserRouter ([
     {
         path: '/',
-        element: <div>Home</div>
+        element: <Home />
     },
+    {
+        path: '/pets',
+        element: <Pets />
+    },
+
+    {
+        path: '/admin',
+        element: <h1>Admin</h1>
+    }
 ])
 
 export default router 
