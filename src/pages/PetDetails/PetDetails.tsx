5d969c0dd156942b90b0287a86a1b83bec6a9813
Tales Porjan <tales.fernandodev@gmail.com>
2024-04-22 21:37:55 -0300

    feat: foi criado uma shelter, um erro para buscar abrigo e atualizado detalhes do pet

--- src/pages/PetDetails/PetDetails.tsx
+++ src/pages/PetDetails/PetDetails.tsx
@@ -1,44 +1,52 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Header } from "../../components/common/Header";
 import { Grid } from "../../components/layout/Grid/grid";
-import styles from './PetDetails.module.css'
+import styles from "./PetDetails.module.css";
 import { useQuery } from "@tanstack/react-query";
 import { getPetById } from "../Pets/getPetById";
 import { ImageBase64 } from "../../components/common/ImageBase64/ImageBase64";
 import Skeleton from "react-loading-skeleton";
 
-export function PetDetails () {
-    const { id } = useParams()
-    const { data, isLoading } = useQuery({
-      queryKey: ['get-pet-by-id', id], 
-      queryFn:  async () => {
-        return await getPetById(id ?? '')
-      },
-    })
-    return (
-        <Grid>
-            <div className={styles.container}>
-            <Header showReturn={true}/>
-            <main className={styles.content}>
-                {isLoading && (
-                  <div className={styles.skeleton}>
-                     <Skeleton circle={true} width={200} height={200} />
-                     <Skeleton width={100} height={24} style={{margin: 16}} />
-                  </div>
-                )}
-                
-                
-                {!isLoading && (
-                  <>
-                    <ImageBase64 src={data?.photo} className={styles.picture} />
+export function PetDetails() {
+  const { id } = useParams();
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["get-pet-by-id", id],
+    queryFn: async () => {
+      return await getPetById(id ?? "");
+    },
+  });
+  return (
+    <Grid>
+      <div className={styles.container}>
+        <Header showReturn={true} />
+        <main className={styles.content}>
+          {isLoading && (
+            <div className={styles.skeleton}>
+              <Skeleton circle={true} width={200} height={200} />
+              <Skeleton width={100} height={24} style={{ margin: 16 }} />
+            </div>
+          )}
+
+          {!isLoading && (
+            <>
+              <ImageBase64 src={data?.photo} className={styles.picture} />
+              {isError && (
+                <>
+                  <h1>Pet não encontrado</h1>
+                  <Link to="/pets/">Voltar para a listagem</Link>
+                </>
+              )}
+              {!isError && (
+                <>
                   <h1>{data?.name}</h1>
                   <span>Sobre o pet:</span>
                   <p>{data?.bio}</p>
-                  </>
-                )}
-                
-            </main>
-            </div>
-        </Grid>
-    )
+                </>
+              )}
+            </>
+          )}
+        </main>
+      </div>
+    </Grid>
+  );
 }
