8eda1ca6b7de3133f311305c4ac3c773bb355181
Tales Porjan <tales.fernandodev@gmail.com>
2024-04-23 22:15:50 -0300

    feat: foi criado um Input para o formulario de pets

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -1,10 +1,18 @@
+import { Button } from "../../../components/common/Button";
 import { Input } from "../../../components/common/Input";
 import { Panel } from "../../../components/layout/Panel";
 
+import styles from './Shelter.module.css'
+
 export function Shelter() {
+    
+
   return (
     <Panel>
+        <form className={styles.container} >
       <Input label="Nome" />
+      <Button type="submit">Salvar dados</Button>
+      </form>
     </Panel>
   );
 }
